567dee80151d5c537bd05985f6d208096cf1c393
105926570 <105926570@student.swin.edu.au>
2025-11-19 15:23:47 +1100

    #2 Created testing Solution

---
+++ UCSL_Testing/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UCSL_Testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
