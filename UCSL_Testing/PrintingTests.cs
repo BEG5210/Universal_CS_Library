f7861eae9844132862ba00c1410dfc82c778d869
105926570 <105926570@student.swin.edu.au>
2025-11-19 16:24:15 +1100

    created test for TestWriteArrayOfLinesToConsole()

--- UCSL_Testing/PrintingTests.cs
+++ UCSL_Testing/PrintingTests.cs
@@ -11,7 +11,18 @@ namespace UCSL_Testing
         [Test]
         public void TestWriteArrayOfLinesToConsole()
         {
+            StringWriter stringWriterMethodOut = new StringWriter(); //Created stringwriter to get strings from console.WriteLine outputs
+            Console.SetOut(stringWriterMethodOut); // Redirect console output to previously defined stringwriter
 
+            WriteArrayOfLinesToConsole(new string[] { "word 1", "word 2"});
+
+
+            StringWriter stringWriterExpectedOut = new StringWriter(); //Created stringwriter to get strings from console.WriteLine outputs
+            Console.SetOut(stringWriterExpectedOut); // Redirect console output to previously defined stringwriter
+
+            Console.WriteLine("word 1\nword 2\n");
+
+            Assert.AreEqual(stringWriterExpectedOut.ToString(), stringWriterMethodOut.ToString());
         }
     }
 }
