d018fcbb5d2170c992a2f0df457aec12a94b01d9
105926570 <105926570@student.swin.edu.au>
2025-11-19 15:15:09 +1100

    Added "WriteArrayOfLinesToConsole"

--- UCSL/Printing.cs
+++ UCSL/Printing.cs
@@ -6,5 +6,17 @@
         {
             Console.WriteLine(message);
         }
+
+        /// <summary>
+        /// Writes each string in the specified array to the console, with each string in the array on a new line.
+        /// </summary>
+        /// <param name="messages">An array of strings to be written to the console. Cannot be null.</param>
+        public void WriteArrayOfLinesToConsole(string[] messages)
+        {
+            foreach (string message in messages)
+            {
+                Console.WriteLine(message);
+            }
+        }
     }
 }
